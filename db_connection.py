8f82e945e479a2cffb65b2beac1b78db8781d820
Camillus83 <kamilrichter98@gmail.com>
2023-01-16 12:46:52 +0100

    Implementation of new class for DB Connection Pool

--- db_connection.py
+++ db_connection.py
@@ -7,9 +7,10 @@ to create and manage pool of database connections using psycopg2 lib.
 The class ensures that only one instance of the connection pool is created.
 """
 import psycopg2.pool
+import psycopg2
 
 
-class DatabaseConnectionMeta(type):
+class DBConnectionPoolMeta(type):
     """
     The Singleton meta class for DatabaseConnection class.
     This meta class ensures that only one instance of the connection pool is created,
@@ -32,101 +33,157 @@ class DatabaseConnectionMeta(type):
         return cls._instances[cls]
 
 
-class DatabaseConnection(metaclass=DatabaseConnectionMeta):
-    """
-    The DatabaseConnection class used to create and manage a pool of database connections
-    using the psycopg2 library.
+class DBConnectionPool(metaclass=DBConnectionPoolMeta):
+    def __init__(self, minconn, maxconn, *args, **kwargs):
 
-    Attributes:
-        minconn(int): minimum number of connections.
-        maxconn(int): maximum number of connections.
-        host(str):
-        db_user(str): username of postgresql user, which will connect to the db.
-        db_password(str): password for the postgresql user.
-        db_name(str): name of the postgresql database.
+        self.minconn = minconn
+        self.maxconn = maxconn
 
-    Methods:
-        __init__ : Initializes the connection pool with given credentials.
-        get_connection() : Returns free connection from the pool.
-        put_away_connection() : Puts away the connection.
-        execute_query() : Execute query during the connection.
-        close_all_connections() : Closing all opened connections.
-    """
+        self._args = args
+        self._kwargs = kwargs
 
-    def __init__(
-        self,
-        minconn: int,
-        maxconn: int,
-        host: str,
-        db_user: str,
-        db_password: str,
-        db_name: str,
-    ):
-        """
-        Creation of a database connection pool.
-        Args:
-            :minconn(int): minimum number of connections.
-            :maxconn(int): maximum number of connections.
-            :host(str):
-            :db_user(str): username of postgresql user, which will connect to the db.
-            :db_password(str): password for the postgresql user.
-            :db_name(str): name of the postgresql database.
-        Returns:
-            None
-        Example:
-             connection1 = DatabaseConnection(1, 10, "localhost", "user", "password", "my_db")
-        """
+        self._pool = []
+        self._used = {}
 
-        self.pool = psycopg2.pool.SimpleConnectionPool(
-            minconn,
-            maxconn,
-            host=host,
-            user=db_user,
-            password=db_password,
-            database=db_name,
-            port=5431,
-        )
+        for i in range(self.minconn):
+            self._connect()
 
-    def get_connection(self) -> "psycopg2.extensions.connection":
-        """
-        get_connection method returns a free connection from the pool.
+    def _connect(self):
+        conn = psycopg2.connect(*self._args, **self._kwargs)
+        self._pool.append(conn)
+        return conn
 
-        Returns:
-            psycopg2.extensions.connection(obj): object created from psycopg2.extensions.connection
-            which represents a single connection. It can be used to execute SQL statements,
-            commit and rollback transactions.
-        """
-        return self.pool.getconn()
+    def get_connection(self):
+        if len(self._pool) > 0:
+            conn = self._pool.pop()
+            self._used[conn] = True
+            # print(self._used)
+            return conn
+        elif len(self._used) < self.maxconn:
+            # print(len(self._used))
+            conn = self._connect()
+            self._pool.remove(conn)
+            self._used[conn] = True
+            return conn
+        else:
+            print("Connection pool exhausted.")
 
-    def put_away_connection(self, connection: "psycopg2.extensions.connection") -> None:
-        """_summary_
+    def put_away_connection(self, conn):
 
-        Args:
-            connection (_type_): _description_
-        """
-        self.pool.putconn(connection)
+        if len(self._pool) < self.minconn:
+            self._pool.append(conn)
+        else:
+            conn.close()
+            del self._used[conn]
 
-    def execute_query(self, query: str) -> None:
-        """
-        execute_query method executes sql query through our database connection.
-        Args:
-            query(str): just SQL query.
-        Returns:
-            None
-        Example:
-            connection1.execute_query("SELECT * FROM table1;")
-        """
-        connection = self.get_connection()
-        cursor = connection.cursor()
-        cursor.execute(query)
-        connection.commit()
-        cursor.close()
-        self.put_away_connection(connection)
+    def print_pool(self):
+        print("Pool:")
+        print(self._pool)
+        print("Len of pool:")
+        print(len(self._pool))
 
-    def close_all_connections(self) -> None:
-        """
-        Closing all existing connections.
-        Returns:
-            None
-        """
-        self.pool.closeall()
+    def print_used(self):
+        print("Used:")
+        print(self._used)
+        print("Len of used:")
+        print(len(self._used))
+
+
+# class DatabaseConnection(metaclass=DatabaseConnectionMeta):
+#     """
+#     The DatabaseConnection class used to create and manage a pool of database connections
+#     using the psycopg2 library.
+
+#     Attributes:
+#         minconn(int): minimum number of connections.
+#         maxconn(int): maximum number of connections.
+#         host(str):
+#         db_user(str): username of postgresql user, which will connect to the db.
+#         db_password(str): password for the postgresql user.
+#         db_name(str): name of the postgresql database.
+
+#     Methods:
+#         __init__ : Initializes the connection pool with given credentials.
+#         get_connection() : Returns free connection from the pool.
+#         put_away_connection() : Puts away the connection.
+#         execute_query() : Execute query during the connection.
+#         close_all_connections() : Closing all opened connections.
+#     """
+
+#     def __init__(
+#         self,
+#         minconn: int,
+#         maxconn: int,
+#         host: str,
+#         db_user: str,
+#         db_password: str,
+#         db_name: str,
+#     ):
+#         """
+#         Creation of a database connection pool.
+#         Args:
+#             :minconn(int): minimum number of connections.
+#             :maxconn(int): maximum number of connections.
+#             :host(str):
+#             :db_user(str): username of postgresql user, which will connect to the db.
+#             :db_password(str): password for the postgresql user.
+#             :db_name(str): name of the postgresql database.
+#         Returns:
+#             None
+#         Example:
+#              connection1 = DatabaseConnection(1, 10, "localhost", "user", "password", "my_db")
+#         """
+
+#         self.pool = psycopg2.pool.SimpleConnectionPool(
+#             minconn,
+#             maxconn,
+#             host=host,
+#             user=db_user,
+#             password=db_password,
+#             database=db_name,
+#             port=5431,
+#         )
+
+#     def get_connection(self) -> "psycopg2.extensions.connection":
+#         """
+#         get_connection method returns a free connection from the pool.
+
+#         Returns:
+#             psycopg2.extensions.connection(obj): object created from psycopg2.extensions.connection
+#             which represents a single connection. It can be used to execute SQL statements,
+#             commit and rollback transactions.
+#         """
+#         return self.pool.getconn()
+
+#     def put_away_connection(self, connection: "psycopg2.extensions.connection") -> None:
+#         """_summary_
+
+#         Args:
+#             connection (_type_): _description_
+#         """
+#         self.pool.putconn(connection)
+
+#     def execute_query(self, query: str) -> None:
+#         """
+#         execute_query method executes sql query through our database connection.
+#         Args:
+#             query(str): just SQL query.
+#         Returns:
+#             None
+#         Example:
+#             connection1.execute_query("SELECT * FROM table1;")
+#         """
+#         connection = self.get_connection()
+#         cursor = connection.cursor()
+#         cursor.execute(query)
+#         connection.commit()
+#         cursor.close()
+#         self.put_away_connection(connection)
+
+#     def close_all_connections(self) -> None:
+#         """
+#         Closing all existing connections.
+#         Returns:
+#             None
+#         """
+#         self.pool.closeall()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 import logging
 from dotenv import load_dotenv
 from db_utils import DatabaseUtility
-from db_connection import DatabaseConnection
+from db_connection import DBConnectionPool
 
 load_dotenv()
 POSTGRES_USER = os.environ["POSTGRES_USER"]
@@ -26,18 +26,67 @@ def main():
     db.container_start()
     db.create_tables()
 
-    connection1 = DatabaseConnection(
-        1, 10, "localhost", POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
+    connection1 = DBConnectionPool(
+        2,
+        3,
+        host="localhost",
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        dbname=POSTGRES_DB,
+        port=5431,
     )
-    connection2 = DatabaseConnection(
-        1, 10, "localhost", POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
+    connection2 = DBConnectionPool(
+        3,
+        10,
+        host="localhost",
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        dbname=POSTGRES_DB,
+        port=5431,
     )
+    connection3 = DBConnectionPool(
+        3,
+        10,
+        host="localhost",
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        dbname=POSTGRES_DB,
+        port=5431,
+    )
+
+    print("after creating pool")
+    connection1.print_pool()
+    connection2.print_used()
+
+    a = connection1.get_connection()
+    print("a: ")
+    print(a)
+
+    connection1.print_pool()
+    connection2.print_used()
+
+    b = connection1.get_connection()
+    print("b: ")
+    print(b)
+
+    connection1.print_pool()
+    connection2.print_used()
+
+    c = connection1.get_connection()
+    print("c: ")
+    print(c)
+
+    connection1.print_pool()
+    connection2.print_used()
 
     log.info("ID of connection1: %s", str(id(connection1)))
     log.info("ID of connection2 %s", str(id(connection2)))
 
-    connection1.execute_query("SELECT * FROM table1;")
-    connection2.execute_query("SELECT * FROM table2;")
+    log.info("Amount of minConn: %s", str(connection1.minconn))
+    log.info("Amount of minConn: %s", str(connection2.minconn))
+
+    # connection1.execute_query("SELECT * FROM table1;")
+    # connection2.execute_query("SELECT * FROM table2;")
 
     # db.drop_tables()
     db.container_stop()
